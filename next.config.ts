eebaf44c13b6d768dc490cead18a13185d8735f9
Graciella <graciellamher@gmail.com>
2026-07-31 02:49:23 +0800

    Bypass NextConfig typescript types to support Next.js 14+ bodySizeLimit syntax

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   serverActions: {
     bodySizeLimit: "100mb",
   },
-};
+  experimental: {},
+} as any;
 
 export default nextConfig;
